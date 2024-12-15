ea638b456a63578a84fa55162c0b50ae2ea46a51
TPWLU <sppaulisto972@gmail.com>
2024-12-15 18:00:04 -0300

    Fix: mapping valid keys

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mapedKeys = [];
 let audio = new Audio("src/assets/tunes/a.wav");
 
 const playTune = (key) => {
@@ -15,6 +16,11 @@ const playTune = (key) => {
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
-document.addEventListener("keydown", (e) => playTune(e.key));
+document.addEventListener("keydown", (e) => {
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+});
