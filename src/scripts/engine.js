4e5a77e2415be6896cf484c0dbb8cc1078c62d9c
TPWLU <sppaulisto972@gmail.com>
2024-12-15 17:40:12 -0300

    Update: playTune to play audio files

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,12 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("tunes");
+let audio = new Audio("src/assets/tunes/a.wav");
 
-const playTune = (key) => {};
+const playTune = (key) => {
+    audio.src = `src/assets/tunes/${key}.wav`;
+    audio.play();
+};
 
-function init() {
-    pianoKeys.forEach((key) => {
-      key.addEventListener("click", () => playTune(key.dataset.key));
-    });
-}
-
-init();
+pianoKeys.forEach((key) => {
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
